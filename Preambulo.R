593b6a82721f07702a822354820337366bfeeea5
MarioDhiego <mario.valente@detran.pa.gov.br>
2024-01-30 12:04:21 -0300

    atualização script

---
+++ Preambulo.R
@@ -0,0 +1,28 @@
+---
+  title: "**Curso De Barbeiro(A) Profissional**"
+subtitle: "Do Zero Absoluto"
+author: "**VIRGÍLIO FERREIRA SOUSA**"
+date: "`r format(Sys.time(), '%d %B, %Y')`"
+toc: true                # Indice/Sumário
+toc-title: SUMÁRIO       # Título do Indice
+toc-location: left       # Posição do Indice
+number-sections: true    # Seção do Indice
+lang: pt                 # idioma: português
+fig-cap-location: top
+format:
+  html: 
+  #code-fold: true 
+  #code-tools: true
+  #code-summary: "Show the code"
+  code-copy: true
+code-line-numbers: true
+html-math-method: katex
+theme: 
+  light: [pulse,custom.scss]  #custom-light.theme
+dark:  [cyborg,custom.scss]  #custom-dark.theme
+title-block-banner: true          #Estilo de banner no topo do documento
+abstract: "O Curso Básico de Barbeiro é indicado para você que é iniciante ou está começando e nunca pegou em uma máquina ou tesoura para cortar cabelo. Todas as aulas foram pensadas e desenvolvidas de uma forma simples e muito fácil de entender para você aprender tudo de forma DESCOMPLICADA e se tornar um barbeiro profissional, mesmo que esteja começando do ABSOLUTO ZERO."
+editor: 
+  markdown: 
+  wrap: 72
+---
